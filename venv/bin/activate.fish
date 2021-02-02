70c446e97481b7e1396b24544dd731d8afd4de36
Audrey Andoy <audrey@Audreys-MacBook-Air.local>
2021-02-02 08:00:18 -0800

    removed unneeded directories

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/audrey/Documents/Ada/audrey-practice/Flask_Migrate/venv"
+set -gx VIRTUAL_ENV "/Users/audrey/Documents/Ada/audrey-practice/flask_workshop_fin/flask_api_db/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
